2129dfedc905c2237769b02e46735e9176ba6a09
전봉철 <bongcheoljeon@ikoob.com>
2026-04-08 16:00:38 +0900

    fix: 타이틀수정

--- src/components/toolbar/AppToolbar.vue
+++ src/components/toolbar/AppToolbar.vue
@@ -5,7 +5,7 @@
         <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
           <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
         </svg>
-        <span class="brand-name">Mermaid <strong>Live</strong></span>
+        <span class="brand-name">Mermaid <strong>에디터</strong></span>
       </div>
     </div>
 
